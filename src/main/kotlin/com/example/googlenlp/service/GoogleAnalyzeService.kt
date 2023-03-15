11addeaa3ff40b4e6d70f21408c4b14001b35514
sjparkk <sjparkk0820@naver.com>
2023-03-15 23:21:34 +0900

    feat : 항목 분석 기능 추가

---
+++ src/main/kotlin/com/example/googlenlp/service/GoogleAnalyzeService.kt
@@ -0,0 +1,33 @@
+package com.example.googlenlp.service
+
+import com.google.cloud.language.v1.*
+import org.springframework.stereotype.Service
+
+@Service
+class GoogleAnalyzeService(
+    val languageServiceClient: LanguageServiceClient
+) {
+
+    /**
+     * Analyze entities text (항목 분석)
+     * @param text
+     */
+    fun analyzeEntitiesText(
+        text: String
+    ): AnalyzeEntitiesResponse {
+
+        languageServiceClient.use { language ->
+
+            val doc = Document.newBuilder().setContent(text)
+                    .setType(Document.Type.PLAIN_TEXT).build()
+
+            val request = AnalyzeEntitiesRequest.newBuilder()
+                    .setDocument(doc)
+                    .setEncodingType(EncodingType.UTF16)
+                    .build()
+
+            return language.analyzeEntities(request)
+        }
+    }
+
+}
