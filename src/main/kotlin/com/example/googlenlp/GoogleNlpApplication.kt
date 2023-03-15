24fe9c15dc3f750879fab6183f3bcca52a0f90dd
sjparkk <sjparkk0820@naver.com>
2023-03-15 21:59:29 +0900

    - git init

---
+++ src/main/kotlin/com/example/googlenlp/GoogleNlpApplication.kt
@@ -0,0 +1,11 @@
+package com.example.googlenlp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleNlpApplication
+
+fun main(args: Array<String>) {
+    runApplication<GoogleNlpApplication>(*args)
+}
